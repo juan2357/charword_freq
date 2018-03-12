31515eb86e44e48ae883e6c5fe323730db55389a
juanfperez <juanfperez@me.com>
2018-03-12 14:22:13 -0400

    header and objectives

---
+++ charword_freq.cpp
@@ -0,0 +1,13 @@
+// Name: Juan Perez   Z#:23026404
+// Course: COP 3014
+// Professor: Dr. Lofton Bullard
+// Due Date: 3/13/18     Due Time: 11:59 pm
+// Total Points: 20
+// Assignment 7: charword_freq.cpp
+//
+// Description:
+// 1. Read the contents of a data file (mytext.dat);
+// 2. Process the frequency of characters (a-z) that appear in the data file 3.
+// Print the frequency of characters in the file to the screen;
+// 4. Be able to use an ifstream object;
+// *************************************************************************************************************************/
